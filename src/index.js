0b7b1f891f2eb9ac3e66788aeab6289e0853247b
faisgit <fa830163@gmail.com>
2025-01-04 13:37:41 +0530

    connect to mogo atlas

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+const app = express()
+import connectDB from "./db/db.js"
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: "./.env"
+})
+connectDB()
+
+
+
+
+// (async () => {
+//     try {
+//        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", (error) => {
+//             console.log(`Error: ${error}`)
+//             throw error
+//        })
+//        const port = process.env.PORT || 5000
+//        app.listen(port, () => {
+//         console.log(`App is running at http://localhost:${port}`)
+//        })
+//     } catch (error) {
+//         console.error("Error: ", error)
+//         throw error
+//     }
+// })()
