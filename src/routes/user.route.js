634fe7757cf91849d207e2a1a8bb85c8633bb411
faisgit <fa830163@gmail.com>
2025-01-27 14:58:34 +0530

    update user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken} from '../controllers/user.controller.js'
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, getUserChannelProfile, getWtachHistory} from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -27,5 +27,12 @@ router.route("/logout").post(
     logoutUser
 )
 router.route("/refresh-token").post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route('/avatar').patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route('/cover-image').patch(verifyJWT, upload.single("/coverImage"))
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route('/history').get(verifyJWT, getWtachHistory)
 
 export default router
